ec7b2d1065700fdd88a74433f64e4c1a1dcd478f
light94 <mishra.rahul1712@gmail.com>
2016-01-24 01:52:22 +0530

    added knn classifier

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import metrics
+from sklearn.cross_validation import train_test_split
+# remove comment from the line below if using ipython notebook
+#%matplotlib inline
 
 data = pd.read_csv('input/exl_train.csv')
 
@@ -21,3 +24,18 @@ data = data.replace({'AGENCY_NAME':agency_mapping})
 y = data['IND_QUOTE_CONVERSION']
 X = data.drop(['IND_QUOTE_CONVERSION','QUOTE_NUM','ORIGINAL_QUOTE_DATE'],axis=1)
 
+# from my gist https://gist.github.com/light94/95e07a651b82bbfdb28e
+
+X_train, X_test, y_train, y_test  = train_test_split(X, y, test_size=0.4)
+
+k_range = range(1,26)
+scores = []
+for k in k_range:
+  knn = KNeighborsClassifier(n_neighbors=k)
+  knn.fit(X_train,y_train)
+  y_pred = knn.predict(X_test)
+  scores.append(metrics.accuracy_score(y_test,y_pred))
+
+plt.plot(k_range,scores)
+plt.xlabel('Value for k in knn')
+plt.ylabel('Testing accuracy')
